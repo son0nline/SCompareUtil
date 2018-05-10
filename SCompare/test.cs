6b21224590d90df03eb76c9008f065ac5f8ede23
son0nline <son0nline@yahoo.com.vn>
2018-05-10 15:54:14 +0700

    some test

--- SCompare/test.cs
+++ SCompare/test.cs
@@ -11,9 +11,58 @@ namespace SCompareUtil
         {
             string str1 = "3.14";
             string str2 = "3.15";
+            log(str1.CompareNumericValueTo(str2), -1);
 
-            //str1.CompareNumericValueTo(str2)
+            str1 = "3.24";
+            str2 = "3.15";
+            log(str1.CompareNumericValueTo(str2), 1);
 
+            str1 = "3.14";
+            str2 = "3.14";
+            log(str1.CompareNumericValueTo(str2), 0);
+
+            str1 = "3.14";
+            str2 = "-3.15";
+            log(str1.CompareNumericValueTo(str2), 1);
+
+            str1 = "-3.14";
+            str2 = "3.15";
+            log(str1.CompareNumericValueTo(str2), -1);
+
+            str1 = "-3.14";
+            str2 = "-3.15";
+            log(str1.CompareNumericValueTo(str2), 1);
+
+            str1 = "-3.24";
+            str2 = "-3.15";
+            log(str1.CompareNumericValueTo(str2), -1);
+
+
+            str1 = "-3.14";
+            str2 = "-3.14";
+            log(str1.CompareNumericValueTo(str2), 0);
+
+            str1 = "3.14";
+            str2 = "31";
+            log(str1.CompareNumericValueTo(str2), -1);
+
+            str1 = "13";
+            str2 = "3.15";
+            log(str1.CompareNumericValueTo(str2), 1);
+
+            str1 = "3.1401";
+            str2 = "3.14";
+            log(str1.CompareNumericValueTo(str2), 1);
+
+
+
+        }
+
+        static int count = 0;
+        static void log(int i, int s)
+        {
+            Console.Write(count + "\t");
+            Console.WriteLine(i == s);
         }
     }
 }
